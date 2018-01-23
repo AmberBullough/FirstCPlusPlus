e4f442540dc952e4b19c2e248c0cf52867b891c5
Amber Bullough <amber@bertbullough.com>
2018-01-23 09:25:21 -0700

    Added ppointers

--- FirstCPlusPlus/Controller/Runner.cpp
+++ FirstCPlusPlus/Controller/Runner.cpp
@@ -6,13 +6,22 @@
 //  Copyright © 2018 CTEC. All rights reserved.
 //
 
-#include "Runner.hpp"
+#include "iostream"
 #include "Controller.hpp"
 
 int main()
 {
-    Controller app;
-    app.start();
+    //Controller app;
+    //app.start();
+    //Another way to do above :::::
+    Controller * app = new Controller();
+    //Dereference pointer
+    
+    (*app).start();
+    // Pointer
+    
+    app ->start();
+    //Pointer selector faster
     
     return 0;
 }
