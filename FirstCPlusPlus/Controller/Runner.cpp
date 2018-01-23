e0593a415192cde53a27cc170f69ae82aa8bd071
Amber Bullough <amber@bertbullough.com>
2018-01-23 09:06:36 -0700

    Added in runner and controller and data

---
+++ FirstCPlusPlus/Controller/Runner.cpp
@@ -0,0 +1,9 @@
+//
+//  Runner.cpp
+//  FirstCPlusPlus
+//
+//  Created by Bullough, Amber on 1/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Runner.hpp"
--- FirstCPlusPlus/main.cpp
+++ FirstCPlusPlus/main.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-int main()
+int oldMain()
 {
     // insert code here...
     std::cout << "Hello, World!" << endl;
